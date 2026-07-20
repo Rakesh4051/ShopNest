02ccd2d9ff540a5139eabae093480e6894b16bd7
Rakesh4051 <rakesh9717350853@gmail.com>
2026-07-21 00:47:25 +0530

    Fix duplicate razorpay import

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -76,8 +76,6 @@ const updateOrderStatus = async (req, res) => {
 
 // RazorPay get way
 
-const Razorpay = require('razorpay');
-
 const razorpayInstance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
